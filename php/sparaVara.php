c98a2c19d2e12618486c61100de29d683a5acfb2
FlaviusDumitru <121314871+FlaviusDumitru@users.noreply.github.com>
2023-05-16 11:08:25 +0300

    Spara vara och grundläggande tester funkar

--- php/sparaVara.php
+++ php/sparaVara.php
@@ -1,3 +1,34 @@
 <?php
 declare (strict_types=1);
-echo "test";
+
+// Inkludera gemensamma funktioner
+require_once "funktioner.php";
+
+// Läs indata och saner
+// Kontrollera metod
+if ($_SERVER['REQUEST_METHOD']!=='POST') {
+    $error = new stdClass ();
+    $error -> meddelande= ["Wrong method", "Sidan ska anropas med POST"];
+    skickaJSON($error, 405);
+}
+
+// Läs indata
+$vara=filter_input(INPUT_POST, 'vara', FILTER_SANITIZE_SPECIAL_CHARS);
+if (!isset($vara) || mb_strlen($vara)>30) {
+    $error = new stdClass();
+    $error -> meddelande=["Bad input", "Parametern 'vara' saknas eller är för lång (max 30 tecken)"];
+    skickaJSON($error,400);
+}
+
+// Koppla mot databas
+$db = connectDB();
+
+// Spara data
+$sql = "INSERT INTO varor (namn) VALUES (:vara)";
+$stmt = $db -> prepare($sql);
+
+$stmt -> execute(['vara'=>$vara]);
+$id = $db->lastInsertId();
+
+// Skicka tillbaka svar
+skickaJSON(['id'=>$id]);
