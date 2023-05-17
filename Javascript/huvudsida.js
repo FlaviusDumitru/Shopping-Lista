b5a4bb1ef88426104ad82aaeb0a11438874672c9
FlaviusDumitru <121314871+FlaviusDumitru@users.noreply.github.com>
2023-05-17 13:01:38 +0300

    Fick tabell att visa det som står i databasen

--- Javascript/huvudsida.js
+++ Javascript/huvudsida.js
@@ -12,18 +12,53 @@ window.onload = function (){
                 }
             })
             .then(function (data) {
+                console.log(data);
                 appendProducts(data);
             })
     }
 
 function appendProducts(data){
-    console.log(data);
+   
     tabell = document.getElementById("varatable");
     tabell.innerHTML="";
-}
+
 
 for (let i = 0; i < data.length; i++) {
-
-
-    }
+        let tr = document.createElement('tr');
+        let td_text = document.createElement ("td");
+        td_text.innerHTML = data[i].namn;
+        
     
+    // Skapar redigera knapp och lägger den i tabellen
+        let td_redigera = document.createElement('td');
+        let redigera = document.createElement('a');
+        redigera.innerHTML = "E";
+        redigera.onclick=function(){
+                redigeraVara(data[i].id)}
+        td_redigera.appendChild(redigera);
+              
+        // Skapar radera knapp och lägger den i tabellen
+        let td_radera = document.createElement('td');
+        let a_radera = document.createElement('a');
+        a_radera.innerHTML = "D";
+        a_radera.onclick=function(){
+            if(confirm("Vill du radera varan?")){
+                saveActivity(tasks[i].id,"radera")}
+            }
+        td_radera.appendChild(a_radera);
+
+                // Visar celler och knappar
+                tr.appendChild(td_text);
+                tr.appendChild(td_redigera);
+                tr.appendChild(td_radera);
+                tabell.appendChild(tr);
+        }
+}
+    // redigerar varan
+    function redigeraVara(id){
+        document.getElementById("item-input").innerHTML="Redigera Vara";
+        document.getElementById("inputKategori").value=document.getElementById("vara" + id).innerHTML;
+        document.getElementById("add-button").onclick=function() {
+            saveActivity(id,"save");
+          }
+    }
