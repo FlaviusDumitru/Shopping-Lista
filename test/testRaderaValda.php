7b3cdf4efacce191f9cef0879245727621f5a9be
FlaviusDumitru <121314871+FlaviusDumitru@users.noreply.github.com>
2023-05-25 12:25:49 +0300

    Test radera enskild vara klart

--- test/testRaderaValda.php
+++ test/testRaderaValda.php
@@ -1,12 +1,27 @@
 <?php
 declare (strict_types = 1);
+require_once  "../php/funktioner.php";
 
 try {
+    // Skapa handle till cUrl för att läsa svaret
+    $ch = curl_init ('http://localhost/Shoppinglista/php/raderaValda.php');
+
+    // Se till att vi får svaret som en sträng
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Anropen till sidan som ska testas
+    // Fel metod
+    echo "<p class = 'info'> Test fel metod</p>";
+    felMetod($ch);
+    
+    // Test ok
+    echo "<p class = 'info'> Test radera valda OK</p>";
+   raderaValda($ch);
 
 } catch (Exception $e) {
     echo "<p class = 'error'>";
-    echo "Något gick JÄTTEfel!<br>";
+    echo "Något gick JÄTTEfell!<br>";
     echo $e -> getMessage ();
+    echo $e->getFile();
     echo "</p>";
 }
-
