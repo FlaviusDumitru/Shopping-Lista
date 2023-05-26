4a7fca1e7c6599fd73e0f39b0c35ba603605b32e
FlaviusDumitru <121314871+FlaviusDumitru@users.noreply.github.com>
2023-05-26 13:43:08 +0300

    lade till så att den raden som blir redigerad ändrar färg

--- Javascript/huvudsida.js
+++ Javascript/huvudsida.js
@@ -1,12 +1,13 @@
 serverurl = "php/"
 
 window.onload = function () {
+
     document.getElementById("add-button").onclick = function () {
         saveProduct();
     }
     getProducts();
 }
-
+// Fetchar back-end funktionen som hämtar alla varor
 function getProducts() {
     fetch(serverurl + 'hamtaAlla.php')
         .then(function (response) {
@@ -22,25 +23,25 @@ function getProducts() {
 
 // sparar vara
 function saveProduct() {
-    if (document.getElementById("item-input").value !=='') {
-    let varor = document.getElementById('item-input').value;
-    let FD = new FormData();
-    FD.append("vara", varor);
+    if (document.getElementById("item-input").value !== '') {
+        let varor = document.getElementById('item-input').value;
+        let FD = new FormData();
+        FD.append("vara", varor);
 
-    // Hämtar och sparar med fetch genom POST
-    fetch(serverurl + 'sparaVara.php',
-        {
-            method: 'POST',
-            body: FD
-        })
-        .then(function (response) {
-            if (response.status == 200) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            getProducts();
-        })
+        // Hämtar och sparar med fetch genom POST
+        fetch(serverurl + 'sparaVara.php',
+            {
+                method: 'POST',
+                body: FD
+            })
+            .then(function (response) {
+                if (response.status == 200) {
+                    return response.json();
+                }
+            })
+            .then(function (data) {
+                getProducts();
+            })
         document.getElementById('item-input').value = '';
     }
 }
@@ -48,22 +49,22 @@ function saveProduct() {
 function raderaVara(id) {
     if (confirm("Vill du radera varan?")) {
 
-    let FD = new FormData();
-    FD.append("id", id);
-    // Hämtar och sparar med fetch genom POST
-    fetch(serverurl + 'raderaVara.php',
-        {
-            method: 'POST',
-            body: FD
-        })
-        .then(function (response) {
-            if (response.status == 200) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            getProducts();
-        })
+        let FD = new FormData();
+        FD.append("id", id);
+        // Hämtar och sparar med fetch genom POST
+        fetch(serverurl + 'raderaVara.php',
+            {
+                method: 'POST',
+                body: FD
+            })
+            .then(function (response) {
+                if (response.status == 200) {
+                    return response.json();
+                }
+            })
+            .then(function (data) {
+                getProducts();
+            })
     }
 
 }
@@ -95,22 +96,23 @@ function appendProducts(data) {
     for (let i = 0; i < data.length; i++) {
         let tr = document.createElement('tr');
 
+        // Skapar namn cell
         let td_text = document.createElement("td");
         td_text.innerHTML = data[i].namn;
-        td_text.id = "vara" + data[i].id; 
+        td_text.id = "vara" + data[i].id;
 
         // Skapar Checkbox cell
         let td_checkbox = document.createElement('td');
         let checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
-        if (data[i].checked){
+        if (data[i].checked) {
             checkbox.checked = 1;
         }
         checkbox.onclick = function () {
             checkaVara(data[i].id);
         }
         td_checkbox.appendChild(checkbox);
-        
+
 
         // Skapar redigera knapp och lägger den i tabellen
         let td_redigera = document.createElement('td');
@@ -128,13 +130,11 @@ function appendProducts(data) {
         a_radera.classList = "material-icons";
         a_radera.innerHTML = "delete";
         a_radera.onclick = function () {
-                raderaVara(data[i].id)
+            raderaVara(data[i].id)
         }
-        td_radera.appendChild(a_radera);
 
-
-        td_radera.appendChild(a_radera);
         // Visar celler och knappar
+        td_radera.appendChild(a_radera);
         tr.appendChild(td_checkbox);
         tr.appendChild(td_text);
         tr.appendChild(td_redigera);
@@ -142,66 +142,67 @@ function appendProducts(data) {
         tabell.appendChild(tr);
     }
 }
-
+// Fetchar back-end funktionen som raderar alla varor
 function raderaAlla() {
     if (confirm("Vill du verkligen radera alla varor?")) {
 
-    fetch(serverurl + 'raderaAllaVaror.php',
-    {
-        method: 'POST'
-    })
-        .then(function (response) {
-            if (response.status == 200) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            getProducts();
-        })
+        fetch(serverurl + 'raderaAllaVaror.php',
+            {
+                method: 'POST'
+            })
+            .then(function (response) {
+                if (response.status == 200) {
+                    return response.json();
+                }
+            })
+            .then(function (data) {
+                getProducts();
+            })
 
+    }
 }
-}
-
+// Fetchar back-end funktionen som raderar de kryssade varorna
 function raderaKryssade() {
     if (confirm("Vill du verkligen radera kryssade varor?")) {
 
-    fetch(serverurl + 'raderaValda.php',
-    {
-        method: 'POST'
-    })
-        .then(function (response) {
-            if (response.status == 200) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            getProducts();
-        })
+        fetch(serverurl + 'raderaValda.php',
+            {
+                method: 'POST'
+            })
+            .then(function (response) {
+                if (response.status == 200) {
+                    return response.json();
+                }
+            })
+            .then(function (data) {
+                getProducts();
+            })
 
-}
+    }
 }
 // sparar vara
 function uppdateraVara(id) {
-    if (document.getElementById("item-input").value !=='') {
-    let varor = document.getElementById('item-input').value;
-    let FD = new FormData();
-    FD.append("vara", varor);
-    FD.append("id", id);
-    
-    // Hämtar och sparar med fetch genom POST
-    fetch(serverurl + 'uppdateraVara.php',
-        {
-            method: 'POST',
-            body: FD
-        })
-        .then(function (response) {
-            if (response.status == 200) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            getProducts();
-        })
+    if (document.getElementById("item-input").value !== '') {
+        let varor = document.getElementById('item-input').value;
+        let FD = new FormData();
+        FD.append("vara", varor);
+        FD.append("id", id);
+
+        // Hämtar och sparar med fetch genom POST
+        fetch(serverurl + 'uppdateraVara.php',
+            {
+                method: 'POST',
+                body: FD
+            })
+            .then(function (response) {
+                if (response.status == 200) {
+                    return response.json();
+                }
+            })
+            .then(function (data) {
+                getProducts();
+            })
+        // Gör så att "add-button" ändras till "lägg till" efter man har uppdaterat varan
         document.getElementById('item-input').value = '';
         document.getElementById('add-button').innerHTML = 'Lägg till';
         document.getElementById("add-button").onclick = function () {
@@ -211,9 +212,17 @@ function uppdateraVara(id) {
 }
 // redigerar varan
 function redigeraVara(id) {
-    document.getElementById("add-button").innerHTML = "Spara Redigering";
-    document.getElementById("item-input").value = document.getElementById("vara" + id).innerHTML;
-    document.getElementById("add-button").onclick = function () {
+    // Genom CSS, gör det så att den raden som blir redigerad ändrar färg då den redigeras.
+    var highlightedRows = document.getElementsByClassName("highlighted-row");
+    for (var i = 0; i < highlightedRows.length; i++) {
+        highlightedRows[i].classList.remove("highlighted-row");
+    }
+    var editedRow = document.getElementById("vara" + id).parentNode;
+    editedRow.classList.add("highlighted-row");
+
+    document.getElementById("add-button").innerHTML = "Spara Redigering"; // Ändrar "add-button" knappen till "Spara redigering"
+    document.getElementById("item-input").value = document.getElementById("vara" + id).innerHTML; // Gör så att det som skrivs i textrutan ändrar "item-input"
+    document.getElementById("add-button").onclick = function () { // När knappen trycks ner körs funktionen
         uppdateraVara(id, "save");
     }
 }
